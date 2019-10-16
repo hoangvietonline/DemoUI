d1f948a9b9b6848b015b90ad0b31326bb56850f7
hoangvietonline <vietndh@maxmoble-sofware.com>
2019-10-16 15:23:03 +0700

    DONE save Frame from Firebase using Asynctask, show dialog percent download

--- app/src/main/java/hoangviet/ndhv/demoui/FrameFragment.java
+++ app/src/main/java/hoangviet/ndhv/demoui/FrameFragment.java
@@ -1,11 +1,11 @@
 package hoangviet.ndhv.demoui;
 
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,24 +13,29 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.transition.Transition;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -41,14 +46,17 @@ import hoangviet.ndhv.demoui.model.Frame;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FrameFragment extends Fragment implements FrameAdapter.onClickItemFrameListener {
+public class FrameFragment extends Fragment implements FrameAdapter.onClickItemFrameListener{
     private static final String TAG = "FrameFragment";
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     private RecyclerView recyclerViewFrame;
     private List<Frame> frameList;
     private FrameAdapter adapter;
     private setTypeFrameListener mListener;
-
+    private ProgressBar progressBar;
+    private TextView txtProgressBar;
+    private TextView txtPercentLoad;
+    private ImageView imgDowdload;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -106,8 +114,18 @@ public class FrameFragment extends Fragment implements FrameAdapter.onClickItemF
                 frameList.get(i).setChooseFrame(false);
             }
         }
-//        saveToInternalStorage(frame);
-        mListener.setTypeFrame(frame.getNameFrame(),frame.getPathFrame());
+
+        ContextWrapper cw = new ContextWrapper(Objects.requireNonNull(getActivity()).getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File file = new File(directory, frame.getNameFrame());
+        if (!file.exists() && position != 0) {
+            String[] pathDown = {frame.getPathFrame(), frame.getNameFrame()};
+            dialogDownload(frame.getThubFrame());
+            new DownloadFileFromUrl().execute(pathDown);
+            mListener.setTypeFrame(frame.getNameFrame());
+            adapter.notifyDataSetChanged();
+        }
+        mListener.setTypeFrame(frame.getNameFrame());
         adapter.notifyDataSetChanged();
     }
 
@@ -117,56 +135,94 @@ public class FrameFragment extends Fragment implements FrameAdapter.onClickItemF
         super.onAttach(context);
     }
 
-    private void saveToInternalStorage(final Frame frame) {
-        dialogDownload();
-        Glide.with(Objects.requireNonNull(getActivity())).asBitmap().load(frame.getPathFrame()).into(new CustomTarget<Bitmap>() {
-            @Override
-            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                ContextWrapper cw = new ContextWrapper(Objects.requireNonNull(getActivity()).getApplicationContext());
-                File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-                File file = new File(directory, frame.getNameFrame());
-                if (!file.exists()) {
-                    Log.d("path", file.toString());
-                    FileOutputStream fos = null;
-                    try {
-                        fos = new FileOutputStream(file);
-                        resource.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                        Toast.makeText(getActivity(), "save image success", Toast.LENGTH_SHORT).show();
-                        fos.flush();
-                        fos.close();
-                        mListener.setTypeFrame(frame.getNameFrame(),frame.getPathFrame());
-                        adapter.notifyDataSetChanged();
-                    } catch (java.io.IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            @Override
-            public void onLoadCleared(@Nullable Drawable placeholder) {
-            }
-        });
-    }
-
-    private void dialogDownload() {
+    @SuppressLint("SetTextI18n")
+    private void dialogDownload(String urithumb) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        View view = getLayoutInflater().inflate(R.layout.dialog_custom, null);
+        @SuppressLint("InflateParams") View view = getLayoutInflater().inflate(R.layout.dialog_custom, null);
         builder.setView(view);
         final AlertDialog dialog = builder.create();
         ImageButton imageButton = view.findViewById(R.id.btnClause);
+        progressBar = view.findViewById(R.id.progressBar);
+        txtPercentLoad = view.findViewById(R.id.txtPercentLoad);
+        imgDowdload = view.findViewById(R.id.imgImageDownload);
+        imgDowdload.setScaleType(ImageView.ScaleType.FIT_XY);
+        Glide.with(Objects.requireNonNull(getActivity())).load(urithumb).into(imgDowdload);
+        progressBar.setMax(100);
+        txtProgressBar = view.findViewById(R.id.txtDialog);
+        txtProgressBar.setText("download");
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
             }
         });
-
         dialog.show();
     }
 
+
+
     interface setTypeFrameListener {
-        void setTypeFrame(String type,String pathFrame);
+        void setTypeFrame(String type);
     }
 
+    @SuppressLint("StaticFieldLeak")
+    class DownloadFileFromUrl extends AsyncTask<String, String, String>  {
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            txtPercentLoad.setText(0+"%");
+            progressBar.setProgress(0);
+            txtProgressBar.setText("Downloading file. Please wait...");
+        }
+
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected String doInBackground(String... path) {
+            int count;
+            try {
+                URL url = new URL(path[0]);
+                URLConnection connection = url.openConnection();
+                int lengOfFile = connection.getContentLength();
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+                ContextWrapper cw = new ContextWrapper(Objects.requireNonNull(getActivity()).getApplicationContext());
+                File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+                File file = new File(directory, path[1]);
+                if (!file.exists()) {
+                    OutputStream output = new FileOutputStream(file);
+                    byte[] data = new byte[1024];
+                    long total = 0;
+                    while ((count = input.read(data)) != -1) {
+                        total += count;
+                        int temp = (int) ((total * 100) / lengOfFile);
+                        publishProgress(""+ temp);
+                        // writing data to file
+                        output.write(data, 0, count);
+                    }
+                    output.flush();
+                    output.close();
+                    input.close();
+                    mListener.setTypeFrame(path[1]);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(Integer.parseInt(values[0]));
+            txtPercentLoad.setText(values[0] + "%");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            txtProgressBar.setText("Finish");
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
--- app/src/main/java/hoangviet/ndhv/demoui/MirrorActivity.java
+++ app/src/main/java/hoangviet/ndhv/demoui/MirrorActivity.java
@@ -2,8 +2,6 @@ package hoangviet.ndhv.demoui;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,7 +13,6 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -45,15 +42,11 @@ import com.bumptech.glide.request.target.Target;
 
 import org.wysaid.nativePort.CGENativeLibrary;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -61,9 +54,6 @@ import java.util.Objects;
 public class MirrorActivity extends AppCompatActivity implements MirrorFragment.OnClickTypeMirror, FrameFragment.setTypeFrameListener, FilterFragment.OnTypeFilterListener {
     private static final String TAG = "MirrorActivity";
     private static final int REQUEST_CODE_FILE = 124;
-    private ProgressDialog pDialog;
-    private static final int progressBar_type = 0;
-    private String uriFrame;
     public CGENativeLibrary.LoadImageCallback mLoadImageCallback = new CGENativeLibrary.LoadImageCallback() {
         @Override
         public Bitmap loadImage(String name, Object arg) {
@@ -85,13 +75,14 @@ public class MirrorActivity extends AppCompatActivity implements MirrorFragment.
 
         }
     };
+    public ProgressBar progressBarFilter;
     private File new_file;
     private TabLayout tabLayoutMirror;
     private CustomView customView;
     private TextView txtTitleToobar;
     private Bitmap bitmapResource, bitmapCustom;
-    private TextView tabOne,tabTwo,tabThree;
-    public ProgressBar progressBarFilter;
+    private TextView tabOne, tabTwo, tabThree;
+
     @SuppressLint({"ClickableViewAccessibility", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -173,13 +164,12 @@ public class MirrorActivity extends AppCompatActivity implements MirrorFragment.
                     tabOne.setTextColor(R.color.colorBlack);
                 }
             }
+
             @Override
             public void onPageScrollStateChanged(int i) {
 
             }
         });
-
-        new DownloadFileFromURL().execute(uriFrame);
     }
 
     @SuppressLint("InflateParams")
@@ -232,9 +222,8 @@ public class MirrorActivity extends AppCompatActivity implements MirrorFragment.
     }
 
     @Override
-    public void setTypeFrame(String type,String uri) {
+    public void setTypeFrame(String type) {
         customView.setTypeFrame(type);
-        uriFrame = uri;
     }
 
     @Override
@@ -355,103 +344,5 @@ public class MirrorActivity extends AppCompatActivity implements MirrorFragment.
         }
     }
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case progressBar_type: // we set this to 0
-                pDialog = new ProgressDialog(this);
-                pDialog.setMessage("Downloading file. Please wait...");
-                pDialog.setIndeterminate(false);
-                pDialog.setMax(100);
-                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                pDialog.setCancelable(true);
-                pDialog.show();
-                return pDialog;
-            default:
-                return null;
-        }
-    }
-    class DownloadFileFromURL extends AsyncTask<String, String, String> {
 
-        /**
-         * Before starting background thread Show Progress Bar Dialog
-         */
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            showDialog(progressBar_type);
-        }
-
-        /**
-         * Downloading file in background thread
-         */
-        @Override
-        protected String doInBackground(String... f_url) {
-            int count;
-            try {
-                URL url = new URL(f_url[0]);
-                URLConnection conection = url.openConnection();
-                conection.connect();
-
-                // this will be useful so that you can show a tipical 0-100%
-                // progress bar
-                int lenghtOfFile = conection.getContentLength();
-
-                // download the file
-                InputStream input = new BufferedInputStream(url.openStream(),
-                        8192);
-
-                // Output stream
-                OutputStream output = new FileOutputStream(Environment
-                        .getExternalStorageDirectory().toString()
-                        + "/2011.kml");
-
-                byte data[] = new byte[1024];
-
-                long total = 0;
-
-                while ((count = input.read(data)) != -1) {
-                    total += count;
-                    // publishing the progress....
-                    // After this onProgressUpdate will be called
-                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
-
-                    // writing data to file
-                    output.write(data, 0, count);
-                }
-
-                // flushing output
-                output.flush();
-
-                // closing streams
-                output.close();
-                input.close();
-
-            } catch (Exception e) {
-                Log.e("Error: ", e.getMessage());
-            }
-
-            return null;
-        }
-
-        /**
-         * Updating progress bar
-         */
-        protected void onProgressUpdate(String... progress) {
-            // setting progress percentage
-            pDialog.setProgress(Integer.parseInt(progress[0]));
-        }
-
-        /**
-         * After completing background task Dismiss the progress dialog
-         **/
-        @Override
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog after the file was downloaded
-            dismissDialog(progressBar_type);
-
-        }
-
-    }
-
-    }
+}
